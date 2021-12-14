c353db5a6a4633e17d218c4233b8ed1658bb78c9
Edgar Ortega <edort93@gmail.com>
2021-12-13 22:55:53 -0600

    updated component names

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import NavTabs from './NavTabs';
+import Navigation from './Navigation';
 import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
 import Resume from './pages/Resume';
 import Contact from './pages/Contact';
 
 
-export default function PortfolioContainer() {
+export default function Header() {
   const [currentPage, setCurrentPage] = useState('Home');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
@@ -28,7 +28,7 @@ export default function PortfolioContainer() {
   return (
     <div>
       {/* We are passing the currentPage from state and the function to update it */}
-      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* Here we are calling the renderPage method which will return a component  */}
       {renderPage()}
     </div>
---
+++ src/components/pages/Project.js
@@ -0,0 +1,11 @@
+
+export default function Project() {
+    // return (
+    // //   <div>
+    // //     <h1>Recent Projects</h1>
+    // //     <p>
+    // //    Portofolio here
+    // //     </p>
+    // //   </div>
+    // );
+  }
