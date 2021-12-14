a9c5b70bca71b1c35e3bdf31183f6a6195903b2c
Edgar Ortega <edort93@gmail.com>
2021-12-13 22:23:15 -0600

    added cleaning up contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,25 +1,4 @@
-// import React from 'react';
 
-// export default function Contact() {
-//   return (
-//     <div>
-//       <h1>Contact Page</h1>
-//       <p>
-//         Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-//         molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-//         magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-//         efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-//         mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-//         posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-//         faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-//         ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-//         dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-//         conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-//         rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-//       </p>
-//     </div>
-//   );
-// }
 import React, { useState } from 'react';
 // import './style.css';
 
@@ -40,7 +19,7 @@ function Contact() {
     const inputType = target.name;
     const inputValue = target.value;
 
-    // Based on the input type, we set the state of either email, username, and password
+    // Based on the input type, we set the state of either email, name, and message
     if (inputType === 'email') {
       setEmail(inputValue);
     } else if (inputType === 'FullName') {
@@ -54,20 +33,14 @@ function Contact() {
     // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
 
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    // First we check to see if the email is not valid or if the Name is empty. If so we set an error message to be displayed on the page.
     if (!validateEmail(email) || !FullName) {
       setErrorMessage('Email or username is invalid');
       // We want to exit out of this code block if something is wrong so that the user can correct it
       return;
       // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
     }
-    // if (!checkPassword(password)) {
-    //   setErrorMessage(
-    //     `Choose a more secure password for the account: ${FullName}`
-    //   );
-    //   return;
-    // }
-    alert(`Hello ${FullName}`);
+    alert(`Hello ${FullName}, thanks for submitting`);
 
     // If everything goes according to plan, we want to clear out the input after a successful registration.
     setFullName('');
@@ -78,7 +51,8 @@ function Contact() {
 
   return (
     <div>
-      <p>Hello {FullName}</p>
+     <h1>Contact Page</h1>
+      <h2>Hello {FullName}</h2>
       <form className="form">
       <input
           value={FullName}
