f934737ed541b9a7f894f0f96506a15ce768b63f
Edgar Ortega <edort93@gmail.com>
2021-12-15 21:22:25 -0600

    added project component and sample card structure

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,13 +1,81 @@
 import React from 'react';
-// import Project from './Project';
+import Project from "./Project";
+import'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from "react-bootstrap";
+
+//images
+import ticket from "./ticket.png";
+
+
 
 export default function Portfolio() {
   return (
-    <div>
-      <h1>Recent Projects</h1>
-      <p>
-     Portofolio here
-      </p>
-    </div>
+<Container  fluid className="project-section">
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <Project
+              imgPath={ticket}
+              title="Ticket Stack Project"
+              deploy="https://customer-service-dashboard.herokuapp.com/"
+              link="https://github.com/justinkemp10/customer_service_portal"
+            />
+          </Col>
+
+          <Col md={4}  className="project-card">
+          <Project
+              imgPath={ticket}
+              title="Ticket Stack Project"
+              deploy="https://customer-service-dashboard.herokuapp.com/"
+              link="https://github.com/justinkemp10/customer_service_portal"
+            />
+          </Col>
+
+          <Col md={4}  className="project-card">
+          <Project
+              imgPath={ticket}
+              title="Ticket Stack Project"
+              deploy="https://customer-service-dashboard.herokuapp.com/"
+              link="https://github.com/justinkemp10/customer_service_portal"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+          <Project
+              imgPath={ticket}
+              title="Ticket Stack Project"
+              deploy="https://customer-service-dashboard.herokuapp.com/"
+              link="https://github.com/justinkemp10/customer_service_portal"
+            />
+          </Col>
+
+          <Col md={4}  className="project-card">
+          <Project
+              imgPath={ticket}
+              title="Ticket Stack Project"
+              deploy="https://customer-service-dashboard.herokuapp.com/"
+              link="https://github.com/justinkemp10/customer_service_portal"
+            />
+          </Col>
+
+          <Col className="project-card">
+          <Project
+              imgPath={ticket}
+              title="Ticket Stack Project"
+              deploy="https://customer-service-dashboard.herokuapp.com/"
+              link="https://github.com/justinkemp10/customer_service_portal"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+    
   );
 }
+
