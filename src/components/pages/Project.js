4f3bebdeb6b36d077121cb268d17b0a156623074
Edgar Ortega <edort93@gmail.com>
2021-12-15 21:23:42 -0600

    imported card button, created project component to create card

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,11 +1,20 @@
+import {Card, Button} from 'react-bootstrap';
 
-export default function Project() {
-    // return (
-    // //   <div>
-    // //     <h1>Recent Projects</h1>
-    // //     <p>
-    // //    Portofolio here
-    // //     </p>
-    // //   </div>
-    // );
-  }
+function Project(props) {
+  return (
+    <Card className="project-card-view">
+      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Body>
+        <Card.Title>{props.title}</Card.Title>
+    
+          <Button variant="primary" href={props.deploy} target="_blank">
+        
+        </Button>
+        <Button variant="secondary" href={props.link} target="_blank">
+        
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+export default Project;
