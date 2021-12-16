ddb3ce5fb6fee231f7624c76a33eea1230844ea5
Edgar Ortega <edort93@gmail.com>
2021-12-15 22:50:23 -0600

    changed text of buttons

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -10,10 +10,10 @@ function Project(props) {
         <Card.Title>{props.title}</Card.Title>
     <ButtonGroup size="sm" >
           <Button variant="primary" href={props.deploy} target="_blank">
-          See App!
+          Launch
         </Button>
         <Button variant="secondary" href={props.link} target="_blank">
-       Check the Code!
+       Github
         </Button>
         </ButtonGroup>
       </Card.Body>
