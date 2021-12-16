678f860fc00cfbe3c2ce3c80d1941fa4dda8b471
Edgar Ortega <edort93@gmail.com>
2021-12-15 22:42:36 -0600

    imported button group and resized and added text

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,18 +1,21 @@
-import {Card, Button} from 'react-bootstrap';
+import {Card, Button,ButtonGroup} from 'react-bootstrap';
+
+
 
 function Project(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Img variant="top" src={props.imgPath}  alt="card-img"  />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
-    
+    <ButtonGroup size="sm" >
           <Button variant="primary" href={props.deploy} target="_blank">
-        
+          See App!
         </Button>
         <Button variant="secondary" href={props.link} target="_blank">
-        
+       Check the Code!
         </Button>
+        </ButtonGroup>
       </Card.Body>
     </Card>
   );
