495de87b8eb65c5c4be51d85d6a5f3b7f1ffeb8a
Edgar Ortega <edort93@gmail.com>
2021-12-16 18:42:13 -0600

    added footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-const App = () => <Header />;
+const App = () => 
+    <div><Header />
+<Footer />
+</div>;
+
 
 export default App;
