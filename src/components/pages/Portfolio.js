34bedb3e08fc9455e87d6d20ef30b3ba5e780712
Edgar Ortega <edort93@gmail.com>
2021-12-17 00:04:17 -0600

    changed text color

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -20,7 +20,7 @@ export default function Portfolio() {
         <h1 className="project-heading">
           My Recent <strong className="purple"> Works </strong>
         </h1>
-        <p style={{ color: "grey" }}>
+        <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
