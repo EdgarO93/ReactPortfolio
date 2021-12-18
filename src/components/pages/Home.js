2faf3fe489366687d45c0a64f9bfd35b1da52022
Edgar Ortega <edort93@gmail.com>
2021-12-17 23:58:20 -0600

    updated layout and image

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import me from './assets/headshot-edgar.jpeg'
-import {Image} from 'react-bootstrap';
+import {Image,Container,Row} from 'react-bootstrap';
 
 export default function Home() {
   return (
-    <div>
+
+
+    <Container fluid>
+      <Row>
       <h1>About Me Page</h1>
-      <Image src= {me} thumbnail style={{maxHeight:"10rem"}}/>
+      </Row>
+      <div>
+      <Image src= {me} roundedCircle style={{maxHeight:"20rem"}}/>
+      </div>
       <p>
       Hi, my name is Edgar. I'm from Chicago. I graduated from American
             University. I hope to improve my diverse professional experience with new
             skills in web development. I am currently enrolled in the Northwestern
             Bootcamp. I am excited to build new projects! This portfolio was built with React!
       </p>
-    </div>
+    </Container>
   );
 }
