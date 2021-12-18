c575dc306d201ceda07275547d939e4492c364e7
Edgar Ortega <edort93@gmail.com>
2021-12-18 00:44:15 -0600

    added list and updated layout

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import {Container,Row,ListGroup,Card,Button} from 'react-bootstrap';
 
 export default function Resume() {
   return (
-    <div>
+    <Container  fluid="md">
+      <Row>
       <h1>Resume</h1>
-      <a href= "src/components/pages/assets/Edgar-Ortega-Resume-2021.pdf" download> Download Resume Here </a>
-      
-    </div>
+      </Row> 
+      <Button variant="outline-info" href= "src/components/pages/assets/Edgar-Ortega-Resume-2021.pdf" download>  Download Resume Here </Button>
+      <Card style={{ width: '18rem' }}>
+      <Card.Title>Proficiencies </Card.Title>
+      <ListGroup variant ="flush">
+  <ListGroup.Item action variant="light"> HTML </ListGroup.Item>
+  <ListGroup.Item action variant="light">CSS</ListGroup.Item>
+  <ListGroup.Item action variant="light">JavaScript</ListGroup.Item>
+  <ListGroup.Item action variant="light">MongoDB</ListGroup.Item>
+  <ListGroup.Item action variant="light">Express</ListGroup.Item>
+  <ListGroup.Item action variant="light">Node.js</ListGroup.Item>
+  <ListGroup.Item action variant="light" >React</ListGroup.Item>
+  <ListGroup.Item action variant="light" >MySQL</ListGroup.Item>
+</ListGroup>
+</Card>
+    </Container>
   );
 }
