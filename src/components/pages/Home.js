00db608c2ca53d82de8ee1ec8977aff03015a530
Edgar Ortega <edort93@gmail.com>
2021-12-18 11:48:21 -0600

    changing the display of about me

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import me from './assets/headshot-edgar.jpeg'
-import {Image,Container,Row} from 'react-bootstrap';
+import {Image,Container,Row,Col} from 'react-bootstrap';
 
 export default function Home() {
   return (
@@ -10,15 +10,20 @@ export default function Home() {
       <Row>
       <h1>About Me Page</h1>
       </Row>
-      <div>
+      <Row style={{display:"flex"}}>   
+      <Col>
       <Image src= {me} roundedCircle style={{maxHeight:"20rem"}}/>
-      </div>
+      </Col>
+      <Col>
       <p>
       Hi, my name is Edgar. I'm from Chicago. I graduated from American
             University. I hope to improve my diverse professional experience with new
             skills in web development. I am currently enrolled in the Northwestern
             Bootcamp. I am excited to build new projects! This portfolio was built with React!
       </p>
+      </Col>
+      </Row>
+
     </Container>
   );
 }
